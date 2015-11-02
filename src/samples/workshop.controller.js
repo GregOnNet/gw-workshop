2f9be3ff33522d85d0bf09ec82058ae1dc63cbec
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-11-02 11:27:28 +0100

    Adds how to di sample

--- src/samples/workshop.controller.js
+++ src/samples/workshop.controller.js
@@ -5,7 +5,9 @@
     .module('workshop')
     .controller('workshopController', WorkshopController);
 
-  function WorkshopController() {
+  WorkshopController.$inject = ['$scope'];
+
+  function WorkshopController($scope) {
     // this -> Zugriff auf den Scope
     this.test = 'Hallo Welt';
     this.attendees = ['Jana', 'Marc', 'Matthias', 'Linda'];
