08f1a2c206cd0ac88d46fa82b81cb6b37076a691
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-11-02 15:07:38 +0100

    Introduces directive with shared scope

---
+++ src/samples/workshop.directive.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('workshop')
+    //         <ws-directive></ws-directive>
+    //         <ws:directive></ws:directive>
+    .directive('wsDirective', wsDirective);
+
+    function wsDirective() {
+      return {
+        templateUrl: './samples/workshop.directive.tpl.html',
+        transclude: true
+      };
+    }
+}());
