638bc9332930d7fab421203a2cf19e4f6c212971
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-11-03 09:22:35 +0100

    Introduces $watch and one-time-bindings

--- src/samples/workshop.controller.js
+++ src/samples/workshop.controller.js
@@ -5,25 +5,36 @@
     .module('workshop')
     .controller('workshopController', WorkshopController);
 
-  WorkshopController.$inject = ['$interpolate', 'attendeesFactory', 'attendeesService'];
+  WorkshopController.$inject = ['$interpolate', '$scope' , 'attendeesFactory', 'attendeesService'];
 
-  function WorkshopController($interpolate, attendeesFactory, attendeesService) {
+  function WorkshopController($interpolate, $scope, attendeesFactory, attendeesService) {
     var vm = this;
     // this -> Zugriff auf den Scope
     vm.test = 'Hallo Welt';
-    vm.name = 'Gregor';
     vm.attendees = attendeesFactory.getAll();
     vm.firstAttendee = attendeesService.getFirst();
-    
+
     vm.date = new Date();
 
+
+
+    vm.name = 'Gregor';
     vm.expression = $interpolate('{{ name | uppercase }}');
-    vm.uppercasedName = vm.expression({ name: vm.name });
+
+    $scope.$watch(
+      // Watch Function
+      function() { return vm.name; },
+      // Listener Function
+      function(newValue) {
+        vm.uppercasedName = vm.expression({ name: newValue});
+      }
+    );
+
 
     vm.greet = greet;
 
     function greet() {
-        alert(vm.test + ' ' + vm.name)
+        alert(vm.test + ' ' + vm.name);
     }
   }
 }());
